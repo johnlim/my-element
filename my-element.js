b208c645a8aa7c032a8582cc46313de999a8c5cb
johnlim <johnlim.dingwei@gmail.com>
2018-09-21 17:19:13 +0800

    feat(auth0): Working example that will trigger Lock.

--- my-element.js
+++ my-element.js
@@ -1,19 +1,30 @@
 import {LitElement, html} from '@polymer/lit-element';
-// import auth0 from 'auth0-js'
-import Auth0Lock from 'auth0-lock'
-// new Auth0Lock()
+import 'auth0-element/auth0-auth'
+
 class MyElement extends LitElement {
 
   // Public property API that triggers re-render (synced with attributes)
   static get properties() {
     return {
       foo: String,
-      whales: Number
+      whales: Number,
+      auth0Options: Object
     }
   }
 
   constructor() {
     super();
+    this.auth0Options = {
+      auth: {
+        scope: 'openid email',
+        redirectUri: window.location.origin + '/auth0-element/components/auth0-element/demo/',
+        responseType: 'token',
+        connection: 'Username-Password-Authentication',
+
+      },
+      allowedConnections: ['Username-Password-Authentication'],
+      closable: false
+    };
     this.foo = 'foo';
     this.addEventListener('click', async (e) => {
       this.whales++;
@@ -23,8 +34,16 @@ class MyElement extends LitElement {
   }
 
   // Render method should return a `TemplateResult` using the provided lit-html `html` tag function
-  _render({foo, whales}) {
+  _render() {
     return html`
+      <auth0-auth id="auth0" 
+                  client-id="LP1BxaYQm2NQTZG64MQqHmI2xxDiRMaf" 
+                  domain="johnlim.au.auth0.com" options="${this.auth0Options}" 
+                  logout-redirect-to="${this.auth0Options.redirectUri}" 
+                  user-profile="{{user}}" 
+                  id-token="{{idToken}}" 
+                  query-params="{{params}}" jwt-manager="">
+      </auth0-auth>
       <style>
         :host {
           display: block;
@@ -33,8 +52,8 @@ class MyElement extends LitElement {
           display: none;
         }
       </style>
-      <h4>Foo: ${foo}</h4>
-      <div>whales: ${'🐳'.repeat(whales)}</div>
+      <h4>Foo: ${this.foo}</h4>
+      <div>whales: ${'🐳'.repeat(this.whales)}</div>
       <slot></slot>
     `;
   }
